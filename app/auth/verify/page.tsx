b8422ea3d632ab28cf7e284d4a1bb0b17a11a929
Boburov Shukurullo <boburovdev@gmail.com>
2025-10-13 05:00:51 +0500

    fixed all errors

--- app/auth/verify/page.tsx
+++ app/auth/verify/page.tsx
@@ -1,12 +1,11 @@
 "use client"
-import banner from "@/public/banner.jpg"
 import Image from "next/image"
 import VerifyCodePage from "../components/VerifyForm"
 
 const page = () => {
   return (
     <div className='bg-purple-200 w-full min-h-screen rounded-xl py-2 px-3 grid grid-cols-2 gap-5'>
-      <Image src={banner} alt="banner" className="h-[98vh] rounded-xl"/>
+      <Image src={"@/public/banner.jpg"} alt="banner" className="h-[98vh] rounded-xl"/>
       <VerifyCodePage/>
     </div>
   )
